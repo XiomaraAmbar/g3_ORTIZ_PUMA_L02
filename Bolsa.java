87c0214fbd1d0b796575c66cd05b599d3b4edec7
XiomaraAmbar <xiomara.ortiz@estudiante.ucsm.edu.pe>
2025-03-26 05:39:30 -0500

    Creación de las clases Bolsa, Chocolatina y Golosina

---
+++ Bolsa.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+public class Bolsa <T> implements Iterable <T>{
+    private ArrayList <T> lista = new ArrayList <T> ();
+    private int tope;
+
+    public Bolsa(int tope){
+        super();
+        this.tope = tope;
+    }
+
+    public void add (T objeto){
+        if (lista.size() >= tope){
+            lista.add(objeto);
+        }else{
+            throw new RuntimeException("No caben más");
+        }
+    }
+    public Iterator <T> iterator(){
+        return lista.iterator();
+    }
+}
