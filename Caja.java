f4b432e6b71a2983e0e7d8e11e661a96fc340797
XiomaraAmbar <xiomara.ortiz@estudiante.ucsm.edu.pe>
2025-03-27 14:45:34 -0500

    Se agrego el atributo color en la clase Caja

--- Caja.java
+++ Caja.java
@@ -1,18 +1,28 @@
 public class Caja<T>{
     private T contenido;
+    private String color;
 
     //Constructor para inicializar el contenido
-    public Caja(T contenido){
+    public Caja(T contenido, String color){
         this.contenido = contenido;
+        this.color = color;
     }
 
-    //Getter
+    //Getters
     public T getContenido(){
         return contenido;
     }
 
-    //Setter
+    public String getColor(){
+        return color;
+    }
+
+    //Setters
     public void setContenido(T contenido){
         this.contenido = contenido;
+    }
+
+    public void setColor(String color){
+        this.color = color;
     }
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         //Crear una instancia de Caja de tipoInteger
-        Caja<Integer> cajaEnteros = new Caja<>(10);
-        System.out.println("Contenido de la caja Enteros: " + cajaEnteros.getContenido());
+        Caja<Integer> cajaEnteros = new Caja<>(10,"Azul");
+        System.out.println("Contenido de la caja " + cajaEnteros.getColor() + " de Enteros: " + cajaEnteros.getContenido());
 
         //Crear una instancia de Caja tipo String
-        Caja<String> cajaCadenas = new Caja<>("Hola mundo");
-        System.out.println("Contenido de la caja Cadenas: " + cajaCadenas.getContenido());
+        Caja<String> cajaCadenas = new Caja<>("Hola mundo","Naranja");
+        System.out.println("Contenido de la caja " + cajaCadenas.getColor() + " de Cadenas: " + cajaCadenas.getContenido());
     }
 }
